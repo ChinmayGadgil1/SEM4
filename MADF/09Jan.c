81a51cd13b5a1b1525d21778e534c985d94cc12d
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2025-01-13 16:51:36 +0530

    do while loop

--- MADF/09Jan.c
+++ MADF/09Jan.c
@@ -29,34 +29,40 @@ void display(){
     printf("-\n");
 }
 
-int partition(int low,int high){
-    int pivot=arr[low];
-    int i=low+1;
-    int j=high;
-    while(i<=j){
-        while(arr[i]<=pivot && i<=high)
+void interchange(int* x,int* y){
+    int temp=*x;
+    *x=*y;
+    *y=temp;
+}
+
+int partition(int low, int high) {
+    int pivot = arr[low];
+    int i = low;
+    int j = high;
+    do {
+        do {
             i++;
-        while(arr[j]>pivot)
+        } while (arr[i] <= pivot);
+        do {
             j--;
+        } while (arr[j] > pivot);
         display();
-        printf("i=%d j=%d pivot=%d\n",i+1,j+1,pivot);
-        if(i<j){
-            int temp=arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;      
+        printf("i=%d j=%d pivot=%d\n", i + 1, j + 1, pivot);
+        if (i < j) {
+            interchange(&arr[i], &arr[j]);
         }
-    }
-    int temp=arr[low];
-    arr[low]=arr[j];
-    arr[j]=temp;
+    } while (i < j);
+    int temp = arr[low];
+    arr[low] = arr[j];
+    arr[j] = temp;
     display();
-printf("i=%d j=%d pivot=%d\n",i+1,j+1,pivot);
-return j;
+    printf("i=%d j=%d pivot=%d\n", i + 1, j + 1, pivot);
+    return j;
 }
 
 void quicksort(int low,int high){
     if(low<high){
-        int pivloc=partition(low,high);
+        int pivloc=partition(low,high+1);
         quicksort(low,pivloc-1);
         quicksort(pivloc+1,high);
     }
@@ -83,7 +89,7 @@ arr[n]=INT_MAX;
 quicksort(0,n-1);
 
 display();
-
+printf("\n\n");
 
 return 0;
 }
