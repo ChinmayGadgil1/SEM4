4b78b9bae72d5c97a608abb7f05e594d4dc37eff
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2025-01-15 08:38:54 +0530

    .

--- MPMC/Chinmay/EXPT2/4.asm
+++ MPMC/Chinmay/EXPT2/4.asm
@@ -3,7 +3,8 @@ section .data
     len equ $ - string
     old db 'Hello'
     new db 'Hi   '
-    
+    newline db 10, 0
+
 section .text
     global _start
 
@@ -27,13 +28,19 @@ rep_loop:
     mov al, [new + esi]     
     mov [string + esi], al  
     inc esi                 
-    loop rep_loop          
+    loop rep_loop           
 
 exit:
     mov eax, 4              
     mov ebx, 1              
     mov ecx, string         
     mov edx, len            
+    int 0x80
+
+    mov eax, 4              
+    mov ebx, 1              
+    mov ecx, newline        
+    mov edx, 1              
     int 0x80
 
     mov eax, 1              
