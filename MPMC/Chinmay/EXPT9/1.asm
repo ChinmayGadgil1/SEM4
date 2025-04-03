628bf65748e7bacc83b7feb53d338ebba4cbfd7c
ChinmayGadgil1 <161110548+ChinmayGadgil1@users.noreply.github.com>
2025-04-03 16:34:46 +0530

    EXPT9 modified

--- MPMC/Chinmay/EXPT9/1.asm
+++ MPMC/Chinmay/EXPT9/1.asm
@@ -9,6 +9,12 @@ msg4 db 'Number not found'
 msg4len equ $-msg4
 newline db '',10 
 n1 equ $-newline
+msg5 db ': not found'
+msg5len equ $-msg5
+msg6 db 'Searching index '
+msg6len equ $-msg6
+msg_found db ': found'
+msg_found_len equ $-msg_found
 
 %macro write 2
     mov eax, 4          
@@ -25,6 +31,18 @@ n1 equ $-newline
     mov edx, %2       
     int 80h              
 %endmacro
+
+section .bss
+arr resb 5
+searche resb 10
+i resb 1
+index resb 2
+element resb 2
+num resb 10
+temp resb 5
+
+section .text
+global _start
 
 input:
     mov byte[i], 0
@@ -46,42 +64,58 @@ ret
 linear_search:
     mov ecx, 5         
     mov esi, 0         
-    label:
-        movzx edi, byte[arr+esi]
-        mov [temp], edi           
-        mov al, [temp]          
-        mov bl, [searche]
-        sub bl, '0'
-        cmp al, bl
-        je l1                    
-        jmp end
-    l1:
-        write msg3, msg3len      
-        add esi, '0'              
-        mov [index], esi
-        write index, 2            
-        write newline, n1
-        mov eax, 1
-        mov ebx, 0
-        int 80h                  
-    end:
-        inc esi                
-        loop label               
-ret
+    
+search_loop:
+   
+    push ecx
+    
+ 
+    write msg6, msg6len
+    add esi, '0'              
+    mov [index], esi
+    write index, 1
+    sub esi, '0' 
+    
+  
+    movzx edi, byte[arr+esi]
+    mov [temp], edi           
+    mov al, [temp]          
+    mov bl, [searche]
+    sub bl, '0'
+    cmp al, bl
+    
 
+    jne near element_not_found
+    
+ 
+    write msg_found, msg_found_len
+    write newline, n1
+    
+   
+    write msg3, msg3len
+    add esi, '0'              
+    mov [index], esi
+    write index, 1
+    write newline, n1
+    
+  
+    mov eax, 1
+    mov ebx, 0
+    int 80h
+    
+element_not_found:
+   
+    write msg5, msg5len
+    write newline, n1
+    
+   
+    pop ecx
+    inc esi
+    dec ecx
+    jnz near search_loop
+    
+    ret
 
-section .bss
-arr resb 5
-searche resb 10
-i resb 1
-index resb 2
-element resb 2
-num resb 10
-temp resb 5
-
-
-section .text
-global _start
 _start:
     write msg1, msg1len           
     call input                 
@@ -92,4 +126,4 @@ _start:
     write newline, n1
     mov eax, 1                
     mov ebx, 0
-    int 80h                         
+    int 80h
